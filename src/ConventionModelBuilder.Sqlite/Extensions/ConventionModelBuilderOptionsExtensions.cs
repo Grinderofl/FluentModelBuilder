f38612d2e0706fd933ac4ac123765fce017b53e3
Nero Sule <nero.sule@stormid.com>
2015-10-02 14:17:18 +0100

    Adds Sqlite

---
+++ src/ConventionModelBuilder.Sqlite/Extensions/ConventionModelBuilderOptionsExtensions.cs
@@ -0,0 +1,13 @@
+﻿using ConventionModelBuilder.Options;
+
+namespace ConventionModelBuilder.Sqlite.Extensions
+{
+    public static class ConventionModelBuilderOptionsExtensions
+    {
+        public static ConventionModelBuilderOptions UseSqlServer(this ConventionModelBuilderOptions options, bool useCoreConventions = true)
+        {
+            options.ConventionSetSource = new SqliteConventionSetSource(useCoreConventions);
+            return options;
+        }
+    }
+}
---
+++ src/ConventionModelBuilder.Sqlite/SqliteConventionSetSource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConventionModelBuilder.Options;
+using ConventionModelBuilder.Sources;
+using Microsoft.Data.Entity.Metadata.Conventions;
+using Microsoft.Data.Entity.Sqlite;
+
+namespace ConventionModelBuilder.Sqlite
+{
+    public class SqliteConventionSetSource : DefaultConventionSetSource
+    {
+        public SqliteConventionSetSource(bool useCoreConventions = true) : base(useCoreConventions)
+        {
+        }
+
+        public override ConventionSet CreateConventionSet(ConventionModelBuilderOptions options)
+        {
+            var baseConventions = base.CreateConventionSet(options);
+            return new SqliteConventionSetBuilder().AddConventions(baseConventions);
+        }
+    }
+}
