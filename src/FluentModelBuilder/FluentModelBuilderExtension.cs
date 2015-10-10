db816d3814d4e874c7bc16d0b415ecd2f7ba44a4
Nero Sule <sulenero@gmail.com>
2015-10-10 15:46:22 +0100

    Starts DI rework

--- src/FluentModelBuilder/FluentModelBuilderExtension.cs
+++ src/FluentModelBuilder/FluentModelBuilderExtension.cs
@@ -1,20 +1,75 @@
-﻿using FluentModelBuilder.Options;
+﻿using System.Collections.Generic;
+using FluentModelBuilder.Options;
 using Microsoft.Data.Entity;
 using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Internal;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Metadata.Conventions.Internal;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.DependencyInjection.Extensions;
 
 namespace FluentModelBuilder
 {
     public class FluentModelBuilderExtension : IDbContextOptionsExtension
     {
+
+
         public FluentModelBuilderExtension(DbContextOptionsBuilder builder, FluentModelBuilderOptions options)
         {
             var internalBuilder = new FluentModelBuilder(options);
             ((IDbContextOptionsBuilderInfrastructure) builder).AddOrUpdateExtension(this);
-            builder.UseModel(internalBuilder.Build());
+            //applier.UseModel(internalBuilder.Build());
         }
 
         public void ApplyServices(EntityFrameworkServicesBuilder builder)
         {
+            var services = builder.GetService();
+            services.Replace(ServiceDescriptor.Scoped<IModelSource, FluentModelSource>());
+        }
+    }
+
+    public class FluentModelSource : ModelSource
+    {
+        private IModelBuilderApplier _applier;
+
+        public FluentModelSource(IDbSetFinder setFinder, ICoreConventionSetBuilder coreConventionSetBuilder, IModelBuilderApplier applier) : base(setFinder, coreConventionSetBuilder)
+        {
+            _applier = applier;
+        }
+
+        protected override void FindSets(ModelBuilder modelBuilder, DbContext context)
+        {
+            base.FindSets(modelBuilder, context);
+            _applier.Apply(modelBuilder);
+        }
+    }
+
+    public interface IModelBuilderApplier
+    {
+        void Apply(ModelBuilder builder);
+    }
+
+    public class ModelBuilderApplier : IModelBuilderApplier
+    {
+        private readonly IEnumerable<IModelBuilderConvention> _conventions;
+
+        public ModelBuilderApplier(IEnumerable<IModelBuilderConvention> conventions)
+        {
+            _conventions = conventions;
+        }
+
+        public void Apply(ModelBuilder builder)
+        {
+            foreach(var convention in _conventions)
+                convention.Apply(builder);
+        }
+    }
+
+    public class EntityAddingConvention : IModelBuilderConvention
+    {
+        public void Apply(ModelBuilder builder)
+        {
+            
         }
     }
 }
