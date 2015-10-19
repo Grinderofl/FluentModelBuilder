fc8c8cbc424888470ed9620dd92b476e2a7dbf57
Nero Sule <nero.sule@stormid.com>
2015-10-19 20:48:13 +0100

    Adds some short syntax

--- src/FluentModelBuilder/Extensions/FluentDbContextOptionsBuilderExtensions.cs
+++ src/FluentModelBuilder/Extensions/FluentDbContextOptionsBuilderExtensions.cs
@@ -1,4 +1,8 @@
-﻿namespace FluentModelBuilder.Extensions
+﻿using System;
+using System.Reflection;
+using FluentModelBuilder.Core.Contributors.Impl;
+
+namespace FluentModelBuilder.Extensions
 {
     public static class FluentDbContextOptionsBuilderExtensions
     {
@@ -12,6 +16,49 @@
             where T : IBuilderExtension, new()
         {
             return builder.WithExtension(new T());
+        }
+
+        public static FluentDbContextOptionsBuilder DiscoverEntities(this FluentDbContextOptionsBuilder builder,
+            Action<DiscoveryEntityContributor> builderAction = null)
+        {
+            return builder.Entities(e => e.Discover(builderAction));
+        }
+
+        public static FluentDbContextOptionsBuilder DiscoverEntitiesFromSharedAssemblies(
+            this FluentDbContextOptionsBuilder builder, Action<DiscoveryEntityContributor> builderAction = null)
+        {
+            return builder.DiscoverEntities(e =>
+            {
+                e.FromSharedAssemblies();
+                builderAction?.Invoke(e);
+            });
+        }
+
+        public static FluentDbContextOptionsBuilder DiscoverOverrides(this FluentDbContextOptionsBuilder builder,
+            Action<DiscoveryOverrideContributor> builderAction = null)
+        {
+            return builder.Overrides(o => o.Discover(builderAction));
+        }
+
+        public static FluentDbContextOptionsBuilder DiscoverOverridesFromSharedAssemblies(
+            this FluentDbContextOptionsBuilder builder, Action<DiscoveryOverrideContributor> builderAction = null)
+        {
+            return builder.DiscoverOverrides(o =>
+            {
+                o.FromSharedAssemblies();
+                builderAction?.Invoke(o);
+            });
+        }
+
+        public static FluentDbContextOptionsBuilder AddAssembly(this FluentDbContextOptionsBuilder builder,
+            Assembly assembly)
+        {
+            return builder.Assemblies(a => a.AddAssembly(assembly));
+        }
+
+        public static FluentDbContextOptionsBuilder AddAssemblyContaining<T>(this FluentDbContextOptionsBuilder builder)
+        {
+            return builder.Assemblies(a => a.AddAssemblyContaining<T>());
         }
     }
 }
---
+++ test/FluentModelBuilder.Tests/DiscoveringFromSharedAssembliesAndSpecifyingMultipleBaseTypesWithShortSyntax.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Linq;
+using FluentModelBuilder.Core.Contributors.Extensions;
+using FluentModelBuilder.Extensions;
+using FluentModelBuilder.InMemory.Extensions;
+using FluentModelBuilder.Tests.Core;
+using FluentModelBuilder.Tests.Entities;
+using FluentModelBuilder.TestTarget;
+using Microsoft.Data.Entity;
+using Xunit;
+
+namespace FluentModelBuilder.Tests
+{
+    public class DiscoveringFromSharedAssembliesAndSpecifyingMultipleBaseTypesWithShortSyntax : TestBase
+    {
+        public DiscoveringFromSharedAssembliesAndSpecifyingMultipleBaseTypesWithShortSyntax(ModelFixture fixture) : base(fixture)
+        {
+        }
+
+        protected override void ConfigureOptions(DbContextOptionsBuilder options)
+        {
+            options.ConfigureModel()
+                .AddAssemblyContaining<EntityBase>()
+                .AddAssemblyContaining<EntityBaseWannabe>()
+                .DiscoverEntitiesFromSharedAssemblies(e => e.BaseType<EntityBase>().BaseType<EntityBaseWannabe>())
+                .DiscoverOverridesFromSharedAssemblies()
+                .WithInMemoryDatabase();
+        }
+
+        [Fact]
+        public void AddsCorrectNumberOfEntities()
+        {
+            Assert.Equal(5, Model.EntityTypes.Count);
+        }
+
+        [Theory]
+        [InlineData(typeof(EntityOne), 0)]
+        [InlineData(typeof(EntityTwo), 1)]
+        [InlineData(typeof(EntityOneWannabe), 2)]
+        [InlineData(typeof(EntityTwoWannabe), 3)]
+        [InlineData(typeof(SingleEntity), 4)]
+        public void AddsCorrectEntities(Type expected, int index)
+        {
+            Assert.Equal(expected, Model.EntityTypes[index].ClrType);
+        }
+
+        [Theory]
+        [InlineData(typeof(int), "Id", 0, 0)]
+        [InlineData(typeof(string), "NotIgnored", 0, 1)]
+        [InlineData(typeof(int), "Id", 1, 0)]
+        [InlineData(typeof(int), "Id", 2, 0)]
+        [InlineData(typeof(string), "LookAtMe", 2, 1)]
+        [InlineData(typeof(int), "Id", 3, 0)]
+        [InlineData(typeof(string), "LookAtMeToo", 3, 1)]
+        [InlineData(typeof(int), "Id", 4, 0)]
+        [InlineData(typeof(DateTime), "DateProperty", 4, 1)]
+        public void MapsProperties(Type type, string name, int entityIndex, int propertyIndex)
+        {
+            Assert.Equal(type, Model.EntityTypes[entityIndex].GetProperties().ElementAt(propertyIndex).ClrType);
+            Assert.Equal(name, Model.EntityTypes[entityIndex].GetProperties().ElementAt(propertyIndex).Name);
+        }
+    }
+}
