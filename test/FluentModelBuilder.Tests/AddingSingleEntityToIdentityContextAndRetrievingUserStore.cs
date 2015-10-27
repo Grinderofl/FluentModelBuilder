cabd5bd3408276bfd210376f1eb8d067f3f5d8af
Nero Sule <nero.sule@stormid.com>
2015-10-27 06:13:00 +0000

    Adds identity tests

---
+++ test/FluentModelBuilder.Tests/AddingSingleEntityToIdentityContextAndRetrievingUserStore.cs
@@ -0,0 +1,66 @@
+using System.Linq;
+using System.Reflection;
+using FluentModelBuilder.Extensions;
+using FluentModelBuilder.InMemory.Extensions;
+using FluentModelBuilder.Tests.Core;
+using FluentModelBuilder.Tests.Entities;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Http.Internal;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Framework.DependencyInjection;
+using Xunit;
+
+namespace FluentModelBuilder.Tests
+{
+    public class AddingSingleEntityToIdentityContextAndRetrievingUserStore : IClassFixture<ModelFixture>
+    {
+        private IModel Model;
+
+        public AddingSingleEntityToIdentityContextAndRetrievingUserStore(ModelFixture fixture)
+        {
+            ConfigureServices(fixture.Services);
+            var manager = fixture.Services.BuildServiceProvider().GetService<UserManager<TestUser>>();
+
+            var storeField = manager.GetType().GetProperty("Store", BindingFlags.NonPublic | BindingFlags.Instance);
+            var userStore = storeField.GetValue(manager);
+            var cast = (UserStore<TestUser, IdentityRole, IdentityContext>) userStore;
+            Model = cast.Context.Model;
+        }
+
+        protected void ConfigureServices(IServiceCollection services)
+        {
+            services.AddEntityFramework().AddDbContext<IdentityContext>(ConfigureOptions).AddInMemoryFluentModelBuilder();
+            services.AddIdentity<TestUser, IdentityRole>().AddEntityFrameworkStores<IdentityContext>().AddDefaultTokenProviders();
+            services.AddScoped<DbContext>(x => x.GetService<IdentityContext>());
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        }
+
+        protected void ConfigureOptions(DbContextOptionsBuilder options)
+        {
+            options.ConfigureModel().Entities(x => x.Add<SingleEntity>()).WithInMemoryDatabase();
+        }
+
+        public class IdentityContext : IdentityDbContext<TestUser>
+        {
+        }
+
+        public class TestUser : IdentityUser
+        {
+        }
+
+        [Fact]
+        public void AddsSingleEntity()
+        {
+            Assert.True(Model.EntityTypes.Any(x => x.ClrType == typeof(SingleEntity)));
+        }
+
+        [Fact]
+        public void AddsAllEntities()
+        {
+            Assert.Equal(7, Model.EntityTypes.Count);
+        }
+    }
+}
